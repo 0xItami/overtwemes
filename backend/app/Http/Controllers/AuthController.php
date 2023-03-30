307284f419dfdf9a299ae990fb0164321e9b0bde
Delight Iworima <iworimadelight9@gmail.com>
2023-03-30 03:18:16 +0100

    setting up user twitter auth endpoints using socialite for
    created migrations, routes, and user models for accessing and processing user auth data

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function redirectToTwitter()
+    {
+        return Socialite::driver('twitter')->redirect();
+    }
+
+    public function handleTwitterCallback()
+    {
+        $user = Socialite::driver('twitter')->user();
+
+        $existingUser = User::where('email', $user->getEmail())->first();
+
+        if($existingUser) {
+            Auth::login($existingUser, true);
+            $token = $existingUser->createToken('authToken')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            $newUser = new User;
+            $newUser->name = $user->getName();
+            $newUser->email = $user->getEmail();
+            $newUser->password = bcrypt('password'); // Set a default password
+            $newUser->save();
+
+            Auth::login($newUser, true);
+            $token = $newUser->createToken('authToken')->accessToken;
+            return response()->json(['token' => $token], 200);
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::get('/auth/twitter', [AuthController::class,'redirectToTwitter'])->name('twitter.auth');
+Route::get('/auth/twitter/callback', [AuthController::class,'handleTwitterCallback'])->name('twitter.callback');
+
+Route::middleware('auth:api')->group(function () {
+    // Your protected routes here
 });

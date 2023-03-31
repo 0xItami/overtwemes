183631cdf2e4a3f20267c51587edda68ca87ca48
Delight Iworima <iworimadelight9@gmail.com>
2023-04-01 00:06:57 +0100

    added laravels socialite auth middleware

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -9,6 +9,11 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('web');
+    }
+
     public function redirectToTwitter()
     {
         return Socialite::driver('twitter')->redirect();
@@ -20,7 +25,7 @@ class AuthController extends Controller
 
         $existingUser = User::where('email', $user->getEmail())->first();
 
-        if($existingUser) {
+        if ($existingUser) {
             Auth::login($existingUser, true);
             $token = $existingUser->createToken('authToken')->accessToken;
             return response()->json(['token' => $token], 200);
@@ -37,11 +42,12 @@ class AuthController extends Controller
         }
     }
 
-    public function testApi(){
+    public function testApi()
+    {
         return json_encode([
-            "status"=>"working",
-            "message"=>"delight fucking built this shit",
-            "code"=>"200",
+            "status" => "working",
+            "message" => "delight fucking built this shit",
+            "code" => "200",
         ]);
     }
 }
